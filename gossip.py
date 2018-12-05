0491f7dc320b55067ed1f4d555801f068143a68a
edhuang24 <edhuang24@gmail.com>
2018-12-04 17:01:38 -0800

    improve interface with more detail in print statements

--- gossip.py
+++ gossip.py
@@ -8,7 +8,6 @@ import os
 import sys
 from termcolor import colored
 import json
-# import state
 import client
 
 # message = [favorite_movie, version_number]
@@ -20,27 +19,36 @@ import client
 # }
 
 STATE = {}
+PREVIOUS_STATE = {}
+MAX_PEERS = 3
 
 app = Flask(__name__, static_folder="/")
 
 @app.route('/gossip', methods=['POST'])
 def gossip():
     new_state = request.data
-    # pdb.set_trace()
     update_state(json.loads(new_state))
     # print(colored("rendering state from server:" + json.dumps(STATE), "red"))
     return jsonify(STATE)
 
 # STATE = state.STATE
-# PORT, PEER_PORT = sys.argv[1], sys.argv[2]
-PORT, PEER_PORT = os.environ.get("PORT"), os.environ.get("PEER_PORT")
+# PORT, PEER_PORTS = sys.argv[1], sys.argv[2]
+PORT, peer_ports = os.environ.get("PORT"), os.environ.get("PEER_PORTS")
+PEER_PORTS = []
+if peer_ports is not None:
+    peer_ports = peer_ports.split(",")
+    PEER_PORTS.append(random.choice(peer_ports))
+    PEER_PORTS.append(random.choice(peer_ports))
+    PEER_PORTS.append(random.choice(peer_ports))
+    PEER_PORTS = list(set(PEER_PORTS))
 
-def build_state(port, peer_port):
+def build_state(port, peer_ports):
     global STATE
     STATE[port] = None
-    if peer_port is not None:
-        STATE[peer_port] = None
-    # pdb.set_trace()
+    if len(peer_ports) > 0:
+        for peer_port in peer_ports:
+            if peer_port is not None:
+                STATE[peer_port] = None
 
 def update_state(data):
     global STATE
@@ -51,13 +59,14 @@ def update_state(data):
             STATE[port] = movie_data
 
 def render_state():
+    global PORT
     global STATE
     for key, val in STATE.items():
         if key is not None and val is not None:
-            print(colored("port {0} likes to watch {1}".format(key, val[0]), "red"))
+            print(colored("coming from {0}: port {1} => {2}".format(PORT, key, val[0]), "red"))
     # print(colored("rendering state from client: " + json.dumps(STATE), "green"))
 
-build_state(PORT, PEER_PORT)
+build_state(PORT, PEER_PORTS)
 
 movies = open("movies.txt", "r").read().split("\n")
 
@@ -72,34 +81,34 @@ def select_books():
     global version_number
     while True:
         try:
-            time.sleep(10)
+            time.sleep(15)
             print("i don't like {0} anymore".format(colored(favorite_movie, "green")))
             favorite_movie = random.choice(movies)
             print("my {0} favorite movie is {1}".format(colored("new", "green"), colored(favorite_movie, "green")))
             version_number += 1
             update_state({PORT: [favorite_movie, version_number]})
-            # for port, movie_data in STATE.items():
-            #     if port == PORT:
-            #         continue
-            #     gossip_response = send_gossip(port, STATE)
-            #     update_state(gossip_response.json())
             render_state()
         except KeyboardInterrupt:
             break
 
 def fetch_state():
     global STATE
+    global PREVIOUS_STATE
     while True:
         try:
             time.sleep(5)
             for port, movie_data in STATE.items():
                 if port == PORT:
                     continue
-                print(colored("fetching update from {0}".format(port)))
+                print(colored("fetching update from {0}".format(port), "yellow"))
                 gossip_response = client.send_gossip(port, STATE)
-                # pdb.set_trace()
                 update_state(gossip_response.json())
-                render_state()
+                if STATE != PREVIOUS_STATE:
+                    print(colored("new update for port {0}".format(port), "blue"))
+                    render_state()
+                    PREVIOUS_STATE = STATE
+                else:
+                    print(colored("no new update from port {0}".format(port), "blue"))
         except KeyboardInterrupt:
             break
 
--- simulator.sh
+++ simulator.sh
@@ -4,21 +4,10 @@ osascript -e "tell application \"Terminal\"
     do script \"cd /Users/ehuang/Desktop/gossip-protocol; export FLASK_APP=gossip.py; PORT=${port} flask run --port=$port;\"
 end tell"
 sleep 5
-port=1025
-peer_port=1024
-osascript -e "tell application \"Terminal\"
-    do script \"cd /Users/ehuang/Desktop/gossip-protocol; export FLASK_APP=gossip.py; PORT=${port} PEER_PORT=$peer_port flask run --port=$port;\"
-end tell"
 
-# for port in {1000..1001}; do
-#   case $port in
-#     $1000)
-#       ;;
-#     $1001)
-#       peer_port=1000
-#       ;;
-#   esac
-#   osascript -e "tell application \"Terminal\"
-#       do script \"cd /Users/ehuang/Desktop/gossip-protocol; export FLASK_APP=gossip.py; PORT=${port} PEER_PORT=$peer_port flask run --port=$port;\"
-#   end tell"
-# done
+for port in {$port..$((port + 5))}; do
+  
+  osascript -e "tell application \"Terminal\"
+      do script \"cd /Users/ehuang/Desktop/gossip-protocol; export FLASK_APP=gossip.py; PORT=${port} PEER_PORT=$peer_port flask run --port=$port;\"
+  end tell"
+done
